e79f4396c4782f105020ce4a683688d8f187264b
lvzhu <lvzhu@youzan.com>
2019-12-18 18:08:24 +0800

    初始化并且

---
+++ spring-boot-tests/spring-boot-lvzhu-tests/src/main/java/com/lvzhu/springboot/lvzhu/mvc/LvzhuMVCApplication.java
@@ -0,0 +1,14 @@
+package com.lvzhu.springboot.lvzhu.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LvzhuMVCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LvzhuMVCApplication.class, args);
+    }
+
+}
